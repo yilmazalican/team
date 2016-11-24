d8f1f173b3ef77982692674f46b9d6a3e7df0a81
yilmazalican <alican.yilmaz@isik.edu.tr>
2016-11-24 04:14:46 +0300

    added iFlat

---
+++ iFlat/Pods/Target Support Files/Pods-iFlat/Pods-iFlat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iFlat : NSObject
+@end
+@implementation PodsDummy_Pods_iFlat
+@end
---
+++ iFlat/Pods/Target Support Files/Pods-iFlatUITests/Pods-iFlatUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_iFlatUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iFlatUITestsVersionString[];
+
