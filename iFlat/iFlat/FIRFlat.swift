5aa3679f4a60d0a8c11f9c99b9affdbdcbe3a273
yilmazalican <alican.yilmaz@isik.edu.tr>
2016-12-12 03:23:42 +0300

    minor

--- iFlat/iFlat/FIRFlat.swift
+++ iFlat/iFlat/FIRFlat.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import Firebase
-
+// deletepicture yap!
 protocol FIRFlatDelegate :class
 {
     func insert(flt:ManipulableFlat!,completion: @escaping (String?) -> ())
@@ -16,6 +16,8 @@ protocol FIRFlatDelegate :class
     func disable(oldUsrEmail:String, newUsr:ManipulableUser!, completion: @escaping (String?) -> ())
     func getFlat(id:String, completion: @escaping (FIRFlat?) -> ())
     func addPictures(imgs:[UIImage], completion: @escaping(String?) -> ())
+    func addPicture(img:UIImage, completion: @escaping(String?) -> ())
+    func deletePicture()
     
 }
 
@@ -26,6 +28,34 @@ protocol FIRFlatDelegate :class
 
 class FIRFlat:FIRFlatDelegate
 {
+    internal func deletePicture() {
+        
+    }
+
+    internal func addPicture(img: UIImage, completion: @escaping (String?) -> ()) {
+        
+    }
+
+    internal func addPictures(imgs: [UIImage], completion: @escaping (String?) -> ()) {
+        
+    }
+
+    internal func getFlat(id: String, completion: @escaping (FIRFlat?) -> ()) {
+        
+    }
+
+    internal func disable(oldUsrEmail: String, newUsr: ManipulableUser!, completion: @escaping (String?) -> ()) {
+        
+    }
+
+    internal func edit(oldFlat: ManipulableFlat, newFlt: ManipulableFlat!, completion: @escaping (String?) -> ()) {
+        
+    }
+
+    internal func insert(flt: ManipulableFlat!, completion: @escaping (String?) -> ()) {
+        
+    }
+
 
 
    
