53fdb86b13d4db2bad589d6bc65e10a0f02683b3
erenay93 <mail@erenay.net>
2016-12-17 02:57:32 +0300

    FilterModel minor fix

--- iFlat/iFlat/FilterModel.swift
+++ iFlat/iFlat/FilterModel.swift
@@ -29,10 +29,13 @@ class FilterModel {
     var parking : Bool?
     var gateKeeper : Bool?
 
-    var priceFrom : Int?
-    var priceTo : Int?
+    var priceFrom : Double?
+    var priceTo : Double?
     
-    init(city:String, capacity:Int, bathroomcount:Int, bedcount:Int, bedroomcount:Int,pool:Bool,internet:Bool,cooling:Bool,heating:Bool,tv:Bool,washingMachine:Bool,elevator:Bool,parking:Bool,gateKeeper:Bool,priceFrom:Int, priceTo:Int) {
+    init(){
+        // will be removed
+    }
+    init(city:String, capacity:Int, bathroomcount:Int, bedcount:Int, bedroomcount:Int,pool:Bool,internet:Bool,cooling:Bool,heating:Bool,tv:Bool,washingMachine:Bool,elevator:Bool,parking:Bool,gateKeeper:Bool,priceFrom:Double, priceTo:Double) {
         
         self.city = city
         self.capacity = capacity
