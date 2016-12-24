83ae3ccfe0ec551409dc45380cdc776f440336e1
erenay93 <mail@erenay.net>
2016-12-25 02:57:29 +0300

    [NEW] FlatProfile Images loaded by async request

---
+++ iFlat/iFlat/FlatImagesCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlatImagesCell.swift
+//  iFlat
+//
+//  Created by Eren AY on 25/12/16.
+//  Copyright © 2016 SE 301. All rights reserved.
+//
+
+import UIKit
+
+class FlatImagesCell: UICollectionViewCell {
+    @IBOutlet var FlatImage: UIImageView!
+    
+}
