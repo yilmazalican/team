665df08b5f18c05882b268b348a46a31dfed5793
erenay93 <mail@erenay.net>
2016-12-26 23:36:17 +0300

    [UPDATE] reservation consraints fixed

--- iFlat/iFlat/ReservationViewController.swift
+++ iFlat/iFlat/ReservationViewController.swift
@@ -6,16 +6,19 @@
 //  Copyright © 2016 SE 301. All rights reserved.
 //
 
-import UIKit
+
 import JTAppleCalendar
 
 class ReservationViewController: UIViewController {
-    @IBOutlet var calendarView: JTAppleCalenderView!
+    @IBOutlet var calendarView: JTAppleCalendarView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView") // Registering your cell is manditory
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,14 +27,38 @@ class ReservationViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+}
+
+extension ReservationViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2016 12 26")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
+            calendar: Calendar.current,
+            generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid,
+            firstDayOfWeek: .sunday)
+        return parameters
     }
-    */
-
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+        let myCustomCell = cell as! CellView
+        
+        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        // Setup text color
+        if cellState.dateBelongsTo == .thisMonth {
+            myCustomCell.dayLabel.textColor = UIColor.black
+        } else {
+            myCustomCell.dayLabel.textColor = UIColor.gray
+        }
+    }
 }
