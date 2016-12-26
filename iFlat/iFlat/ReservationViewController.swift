4acd11cfcb1b71b0ba6838dccdfc5ee250a80374
erenay93 <mail@erenay.net>
2016-12-26 23:12:09 +0300

    [NEW] ReservationViewController created

---
+++ iFlat/iFlat/ReservationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ReservationViewController.swift
+//  iFlat
+//
+//  Created by Eren AY on 26/12/16.
+//  Copyright © 2016 SE 301. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class ReservationViewController: UIViewController {
+    @IBOutlet var calendarView: JTAppleCalenderView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
