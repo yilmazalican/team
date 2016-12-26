ea6df135a2d0dbc68f6f830c1a109a04f41e965d
yilmazalican <alican.yilmaz@isik.edu.tr>
2016-12-26 19:16:24 +0300

    Major

--- iFlat/iFlat/AppDelegate.swift
+++ iFlat/iFlat/AppDelegate.swift
@@ -15,8 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    override init()
+    {
+        super.init()
         FIRApp.configure()
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
