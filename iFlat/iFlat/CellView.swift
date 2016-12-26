e7810213c850835333abdb20d2c74598b77f6668
erenay93 <mail@erenay.net>
2016-12-26 23:06:41 +0300

    [NEW] Reservation CellView Controller vreated

---
+++ iFlat/iFlat/CellView.swift
@@ -0,0 +1,13 @@
+//
+//  CellView.swift
+//  iFlat
+//
+//  Created by Eren AY on 26/12/16.
+//  Copyright © 2016 SE 301. All rights reserved.
+//
+
+import JTAppleCalendar
+class CellView: JTAppleDayCellView {
+    @IBOutlet var dayLabel: UILabel!
+    
+}
