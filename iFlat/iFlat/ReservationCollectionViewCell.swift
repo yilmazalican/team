e7c039aa20cc1070c511309c798ffed838b0b0ce
erenay93 <mail@erenay.net>
2016-12-26 20:28:26 +0300

    minor pbxproj

---
+++ iFlat/iFlat/ReservationCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ReservationCollectionViewCell.swift
+//  iFlat
+//
+//  Created by Eren AY on 26/12/16.
+//  Copyright © 2016 SE 301. All rights reserved.
+//
+
+import UIKit
+
+class ReservationCollectionViewCell: UICollectionViewCell {
+    
+}
