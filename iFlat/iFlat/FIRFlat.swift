26e94350f5f5232534655120cc0486d91319aae7
yilmazalican <alican.yilmaz@isik.edu.tr>
2016-12-31 01:48:25 +0300

    reservationreq OK without push notif

--- iFlat/iFlat/FIRFlat.swift
+++ iFlat/iFlat/FIRFlat.swift
@@ -89,6 +89,8 @@ class FIRFlat:FIRFlatDelegate
             "userId" : flt.userID,
             "city" : flt.city!,
             "address": flt.address!,
+            "disabled": flt.disabled!,
+            "published": flt.published!,
             "title" : flt.title!] as [String : Any]
         //user_flats
         FIRREF.instance().getRef().child("user_flats/" + flt.userID).child(flt.id).setValue(aFlat) { (err1, nil) in
@@ -185,6 +187,11 @@ class FIRFlat:FIRFlatDelegate
             flt.flatCapacity = objdict["capacity"] as? Int
             flt.title = objdict["title"] as? String
             flt.address = objdict["adress"] as? String
+            flt.disabled = objdict["disabled"] as? Bool
+            flt.published = objdict["published"] as? Bool
+            flt.userID = (objdict["userId"] as? String)!
+            flt.city = objdict["city"] as? String
+            flt.id = ss.key
             completion(flt)
         })
         
@@ -214,8 +221,11 @@ class FIRFlat:FIRFlatDelegate
                 flt.washingMachine = objdict["washingMachine"] as? Bool
                 flt.flatCapacity = objdict["capacity"] as? Int
                 flt.title = objdict["title"] as? String
+                flt.address = objdict["adress"] as? String
+                flt.disabled = objdict["disabled"] as? Bool
+                flt.published = objdict["published"] as? Bool
+                flt.userID = (objdict["userId"] as? String)!
                 flt.city = objdict["city"] as? String
-                
                 fltArr.append(flt)
             }
             completion(fltArr)
--- iFlat/iFlat/Issue.swift
+++ iFlat/iFlat/Issue.swift
@@ -40,5 +40,7 @@ class Issue
     
     
     
+    
+    
 
 }
