927e77ed668178a95539b4a057d33ef7cf17fdee
erenay93 <mail@erenay.net>
2016-12-31 04:17:49 +0300

    [UPDATE][MINOR] ListFlat Gui improvements

--- iFlat/iFlat/NumberPlusPicker.swift
+++ iFlat/iFlat/NumberPlusPicker.swift
@@ -28,7 +28,7 @@ class NumberPlusPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSour
 
     
 
-    var pickerElements = ["","1+","2+","3+","4+","5+","6+","7+","8+","9+"]
+    var pickerElements = ["0","1","2","3","4","5","6","7","8","9"]
 
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
